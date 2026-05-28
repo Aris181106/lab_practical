96f33e3bbdffdb17e04e52ea5828ce68368fb0f9
Aris181106 <nvt181106@gmail.com>
2026-05-28 09:06:24 +0700

    [COMPONENT] Create tier 1 components

--- session_04_react_basics/hello-react/src/App.jsx
+++ session_04_react_basics/hello-react/src/App.jsx
@@ -1,20 +1,26 @@
-  import { useState } from "react";
-  import reactLogo from "./assets/react.svg";
-  import viteLogo from "./assets/vite.svg";
-  import heroImg from "./assets/hero.png";
-  import "./App.css";
-  import React from "react";
-  import UserProfile from "./components/UserProfile";
-  import ProductInfo from "./components/ProductInfo";
-  import LifecycleDemo from "./components/LifecycleDemo";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import React from "react";
+import UserProfile from "./components/UserProfile";
+import ProductInfo from "./components/ProductInfo";
+import LifecycleDemo from "./components/LifecycleDemo";
+import GoodCounter from "./components/GoodCounter";
+import BadCounter from "./components/BadCounter";
+import FlowDemo from "./components/FlowDemo";
 
-function App(){
-  return(
-      <>
+function App() {
+  return (
+    <>
       <UserProfile />
       <ProductInfo />
       <LifecycleDemo />
-      </>
+      <GoodCounter />
+      <BadCounter />
+      <FlowDemo />
+    </>
   );
 }
 export default App;
---
+++ session_04_react_basics/hello-react/src/components/FlowDemo.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+function FlowDemo(){
+    console.log("🔄 Component render!");
+    const[step, setStep] = useState(1);
+    return(
+        <div style={{padding : "20px"}}>
+            <h2>Activity Flow</h2>
+            <p>Current step: {step}</p>
+            <button style={{margin : "10px"}} onClick={() => setStep (step + 1)}>
+                Next step →
+            </button>
+            <button style={{margin : "10px"}} onClick={() => setStep(1)}>
+                Reset
+            </button>
+            <div style={{padding : "20px", marginTop : "10px", background : "#ffffff"}}>
+                {step === 1 && <p>First step: Hello!</p>}
+                {step === 2 && <p>Second step: Learning React</p>}
+                {step === 3 && <p>Third step: Understanding useState</p>}
+                {step === 4 && <p>Fourth step: Completed!</p>}
+            </div>
+        </div>
+    );
+}
+
+export default FlowDemo;
